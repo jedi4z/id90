ac7fd3bd84839ac846b5d0c766ec28348ba7f5d8
Jesús Díaz <jesusdiazbc@gmail.com>
2019-04-10 20:59:01 -0300

    Render airlines in index view

--- src/Template/login.php
+++ src/Template/login.php
@@ -3,7 +3,13 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-
+            <select name="airline" id="airline">
+                <?php foreach ($airlines as $airline) : ?>
+                    <option value="<?= $airline['display_name'] ?>">
+                        <?= $airline['display_name'] ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
         </div>
     </div>
 </div>
