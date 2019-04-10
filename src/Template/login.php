c29a7b4bdaac7a947ad58f9932c278a3d9a2f6a5
Jesús Díaz <jesusdiazbc@gmail.com>
2019-04-10 20:35:03 -0300

    Add header & navbar snippets

--- src/Template/login.php
+++ src/Template/login.php
@@ -1,3 +1,11 @@
-<?php echo $foo; ?>
-<br>
-<?php echo $bar; ?>
+<?php include_once 'snippets/header.php'?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+
+        </div>
+    </div>
+</div>
+
+<?php include_once 'snippets/footer.php'?>
